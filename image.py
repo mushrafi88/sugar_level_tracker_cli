770129ea4a7764025148863a891237b3e5e0d81a
Mushrafi Munim <mushrafi88@gmail.com>
2021-08-10 21:36:49 +0600

    update image

---
+++ image.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('sugar_chart.csv')
+df=df.replace('-',np.nan)
+
+Time = ['Fasting','2HAB','2HAL','2HAD']
+
+mean = []
+for i in Time:
+    df[i] = df[i].astype(np.float32)
+    mean.append(df[i].mean().round(1))
+
+time = [8.5,11,14.5,23.75]
+
+df = {
+    'Time' : time,
+    'Sugar level (mmol/L)' : mean
+}
+
+sns.set()
+p = sns.lineplot(x='Time', y='Sugar level (mmol/L)',data=df)
+p.set_xlabel("Time(24H)", fontsize = 15)
+p.set_ylabel("Sugar level (mmol/l)", fontsize = 15)
+p.set_title("Average Sugar level", fontsize = 15)
+plt.savefig('sugar_level.png')
--- sugar-level.sh
+++ sugar-level.sh
@@ -3,15 +3,16 @@
 
 
 #cd $HOME/sugar_level_tracker_cli
-
+mkdir -p output
 #write to csv
 python sugar-level.py $1
 #write to pdf
 
 python latex.py
-
+python image.py
 pdflatex sugar_chart.tex
 
+\cp {sugar_level.png,sugar_chart.pdf,sugar_chart.csv} output/
 
 notify-send -t 9000 --icon=video-television "sugar_chart updated" 
 

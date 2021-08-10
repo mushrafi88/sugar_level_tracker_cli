89a67c0489876e569119f90403b04a2b1c3816ea
Mushrafi Munim <mushrafi88@gmail.com>
2021-08-10 22:04:13 +0600

    update image

--- sugar-level.sh
+++ sugar-level.sh
@@ -2,15 +2,25 @@
 
 
 
-#cd $HOME/sugar_level_tracker_cli
+#cd ~/sugar_level_tracker_cli
 mkdir -p output
 #write to csv
-python sugar-level.py $1
+python sugar-level.py $a
 #write to pdf
 
 python latex.py
 python image.py
-pdflatex sugar_chart.tex
+
+FILE=sugar_level.png
+if [ -f "$FILE" ]; then
+  sed -i 's/%//g' sugar_chart.tex
+fi  
+
+pdflatex  sugar_chart.tex
+
+
+#1. read from last line and dunstify
+#2. do a backup
 
 \cp {sugar_level.png,sugar_chart.pdf,sugar_chart.csv} output/
 
